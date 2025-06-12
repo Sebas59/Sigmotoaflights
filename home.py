c478199285f40780b4dbd1bea0320c93768b2f65
sebastian <labsis@debian.FI-LabSistemas.ucatolica.loc>
2025-06-12 05:06:18 -0500

    Se agrego ruta para crear

--- home.py
+++ home.py
@@ -22,3 +22,47 @@ async def lifespan(app: APIRouter):
 
 templades = Jinja2Templates(directory="templates")
 router = APIRouter(lifespan=lifespan)
+
+
+@router.get("/", response_class=HTMLResponse)
+async def leer_home(request: Request):
+    return templades.TemplateResponse("home.html", {"request": request})
+
+
+@router.get("/usuario/crear", tags=["Usuario"])
+async def usuario_create_html(request: Request, session: AsyncSession = Depends(get_session)):
+    return templades.TemplateResponse("user_create.html", {
+        "request": request,
+        "title": "Crear Vehículo"
+    })
+
+
+@router.post("/usuario/crear", tags=["Usuario"])
+async def create_user(
+    user_data: UserCreate = Depends(user_create_form()),
+    session: AsyncSession = Depends(get_session)
+):
+    try:
+        nuevo_user = await crear_user_db(user_data,session)
+        return RedirectResponse(url="/user_registro", status_code=status.HTTP_303_SEE_OTHER)
+    except HTTPException as e:
+         return templades.TemplateResponse(
+            "user_create.html",
+            {
+                "request": Request(scope={"type": "http"}),
+                "title": "Crear Usuario",
+                "error_message": e.detail,
+            },
+            status_code=e.status_code
+        )
+    except Exception as e:
+        print(f"Error inesperado al crear Usuario: {e}")
+        return templades.TemplateResponse(
+            "user_create.html",
+            {
+                "request": Request(scope={"type": "http"}),
+                "title": "Crear Usuario",
+                "error_message": f"Ocurrió un error inesperado al registrar el Usuario."
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
