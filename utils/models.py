9be6c4e9f462becfad9588bc95b97ae816852fa4
Sala 8 <sala8@FI-LabSistemas.ucatolica.loc>
2025-06-12 10:12:31 -0500

    creacion de las clases necesarias para el trabajo

--- utils/models.py
+++ utils/models.py
@@ -1,2 +1,29 @@
-class Mascotas()
-    nombre
+class Vuelo():
+    __tablename__ = "Vuelo"
+    id_vuelo = Column(Integer, primary_key=True, index=True)
+    Origen = Column(String)
+    Destino = Column(String)
+
+class usuario():
+    __tablename__ = "usuario"
+    id_usuario = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, unique=True, index=True)
+    email = Column(String)
+   
+
+class Reserva():
+    __tablename__ = "Reserva"
+    id_reserva = Column(Integer, primary_key=True, index=True)
+    id_vuelo
+    id_mascota
+    
+
+
+
+class Mascotas(Base):
+    __tablename__ = "Masctota"
+    id_mascota = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, unique=True, index=True)
+    id_vuelo = Column(String)
+    
+    
