d8d8ed21fd92a9153521f8793325bbd66a4e1ae0
labsis <labsis@debian.FI-LabSistemas.ucatolica.loc>
2025-06-12 03:31:31 -0500

    Se agrego carpeta de coneciones

---
+++ connections/connection_db.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from data.models import Vehiculo
+
+load_dotenv()
+
+CLEVER_DB = os.getenv("DATABASE_URL")
+
+if not CLEVER_DB:
+    raise ValueError("La variable de entorno DATABASE_URL no está configurada.")
+
+engine: AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
+
+
